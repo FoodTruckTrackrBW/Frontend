2d062a8c9d16f1cbcf7f90a9a37d6cabce044a58
Drake Alia <DrakeTesla@MacBook-Pro.local>
2020-04-27 14:09:43 -0700

    completed login.js

--- app/src/components/Login.js
+++ app/src/components/Login.js
@@ -1,13 +1,51 @@
-import React from "react";
-import * as yup from "yup";
+// import React from "react";
+import axiosWithAuth from "../utils/axiosWithAuth";
 
+class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      credentials: {
+        username: '',
+        password: ''
+      }
+    }
+  }
 
-const Login = ({ erros, touched, ...props }) => {
+  handleChanges = (e) => {
+    this.setState({
+      credentials: {
+        ...this.state.credentials,
+        [e.target.name]: e.target.value
+      }
+    })
+  }
 
+  handleSubmit = (e) => {
+    e.preventDefault();
+    axiosWithAuth()
+    .post('http://localhost:5000/api/login', this.state.credentials)
+    .then(res => {
+      console.log('HELLO FROM HANDLESUBMIT', res)
+      localStorage.setItem('token', res.data.payload);
+      this.props.history.push("/DinerProfile")
+    })
+    .catch(error => console.log(error));
+  }
+
+  // make a post request to retrieve a token from the api
+  render(){
     return (
-        <div className="login"
-        
-    )
-}
-   
-})
+      <div>
+        <h1>Welcome to Login</h1>
+        <form onSubmit={this.handleSubmit}>
+          <input type="text" onChange={this.handleChanges} name="username" placeholder="username" required maxLength='5' />
+          <input type="password" onChange={this.handleChanges} name="password" placeholder="password" required maxLength='8' />
+          <input type="submit" />
+        </form>
+      </div>
+      );
+    };
+  }
+
+export default Login; 
