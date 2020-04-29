4010d44f1c3df03d202ef9d407d9719ed6ed5012
Drake Alia <DrakeTesla@MacBook-Pro.local>
2020-04-29 13:23:38 -0700

    added couple components

--- app/src/components/TruckCard.js
+++ app/src/components/TruckCard.js
@@ -1,6 +1,15 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { useHistory, useParams } from 'react-router-dom';
+import truckContext from '../contexts/TruckContext';
+
 
 const TruckCard = (props) => {
+    const {trucks, setTrucks} = useContext(truckContext)
+    let {truckid} = useParams()
+    const {push} = useHistory()
+        const toDinerMenu = () =>  {
+        push(`/TruckDetails/${props.truck.id}`)
+    }
 
     return (
         <div className="TruckCard">
@@ -8,6 +17,7 @@ const TruckCard = (props) => {
             <p className="TruckCardInfo">Cuisine Type: {props.truck.cuisine_type}</p>
             <p className="TruckCardInfo">{props.truck.truck_img_url}</p>
             <p className="TruckCardInfo">Depearture Time: {props.truck.departure_time}</p>
+            <button onClick={toDinerMenu}>Menu</button>
         </div>
     )
 }
