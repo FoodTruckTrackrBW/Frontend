c273118fd827b73a4b6e18be9a3e81d9eacc1cdf
Cody <codysolomon15@gmail.com>
2020-04-29 20:53:34 -0700

    fixed conflicts between react 1 and I code is now working and app as intended

--- app/src/components/TruckCard.js
+++ app/src/components/TruckCard.js
@@ -2,10 +2,6 @@ import React, { useContext } from 'react'
 import { useHistory, useParams } from 'react-router-dom';
 import truckContext from '../contexts/TruckContext';
 
-
-import { Link, useParams } from 'react-router-dom'
-import { useHistory } from 'react-router-dom';
-
 const TruckCard = (props) => {
     const {trucks, setTrucks} = useContext(truckContext)
     let {truckid} = useParams()
@@ -13,9 +9,6 @@ const TruckCard = (props) => {
         const toDinerMenu = () =>  {
         push(`/TruckDetails/${props.truck.id}`)
     }
-
-    let {truckid} = useParams()
-    const { push } = useHistory()
 
     const menuRedirect = () => {
         push(`/TruckDetails/${truckid}`)
@@ -27,7 +20,6 @@ const TruckCard = (props) => {
             <p className="TruckCardInfo">Cuisine Type: {props.truck.cuisine_type}</p>
             <p className="TruckCardInfo">{props.truck.truck_img_url}</p>
             <p className="TruckCardInfo">Depearture Time: {props.truck.departure_time}</p>
-            <button onClick={menuRedirect}>MENU</button>
             <button onClick={toDinerMenu}>Menu</button>
         </div>
     )
